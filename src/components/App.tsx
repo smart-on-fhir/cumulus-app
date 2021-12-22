929ebc8770a3cf812054db237efd922d604ea48d
Vladimir Ignatov <vladimir.ignatov@childrens.harvard.edu>
2021-12-22 10:16:18 -0500

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import Header from "./Header";
+import "../styles/main.scss";
+
+
+export default function App() {
+    return (
+        <>
+            <Header />
+            <div id="main">
+                TODO: Body
+            </div>
+        </>
+    )
+}
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import "./Header.scss";
+
+
+export default function Header() {
+    return (
+        <header id="app-header">
+            <div className="container row">
+                <div className="col middle center">Header</div>
+                <div className="col middle center">spacer</div>
+                <div className="col middle center">user</div>
+            </div>
+        </header>
+    )
+}
