018039425c3005bbd5045687d122145bb7b689a7
Vladimir Ignatov <vlad.ignatov@gmail.com>
2022-01-05 06:14:45 -0500

    Initial commit

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,25 @@
 import Header from "./Header";
+import LoginForm from "./LoginForm";
+import {
+    BrowserRouter as Router,    
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
+
 import "../styles/main.scss";
 
 
 export default function App() {
     return (
-        <>
+        <Router>
             <Header />
-            <div id="main">
-                TODO: Body
+            <div id="main" className="col">
+                <Routes>
+                    <Route path="/" element="home" />
+                    <Route path="/login" element={ <LoginForm /> } />
+                </Routes>
             </div>
-        </>
+        </Router>
     )
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,21 @@
 import "./Header.scss";
+import logo from "./logo.png";
 
 
 export default function Header() {
     return (
         <header id="app-header">
-            <div className="container row">
-                <div className="col middle center">Header</div>
-                <div className="col middle center">spacer</div>
-                <div className="col middle center">user</div>
+            <div className="container row p-1">
+                <div className="col pr-1 middle left">
+                    <img src={ logo } width="268"/>
+                </div>
+                <div className="col middle center"/>
+                <div className="col pl-1 middle right">
+                    <div className="nowrap">
+                        <i className="fas fa-user-circle" style={{ fontSize: "200%", verticalAlign: "middle" }} /> <b>Full Name</b>
+                        <a className="ml-1" href="#"><b>SIGN OUT</b></a>
+                    </div>
+                </div>
             </div>
         </header>
     )
