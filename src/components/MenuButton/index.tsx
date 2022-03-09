669542c6677c5b6e0e761510250458ff8d7d033d
Vladimir Ignatov <vlad.ignatov@gmail.com>
2022-03-09 10:24:41 -0500

    Various fixes for safari

--- src/components/MenuButton/index.tsx
+++ src/components/MenuButton/index.tsx
@@ -1,4 +1,4 @@
-import { Component } from "react"
+import { Component, createRef, RefObject } from "react"
 import { classList } from "../../utils";
 import "./MenuButton.scss"
 
@@ -12,6 +12,13 @@ interface MenuButtonProps {
 
 export default class MenuButton extends Component<MenuButtonProps>
 {
+    wrapper: RefObject<HTMLSpanElement>;
+
+    constructor(props: MenuButtonProps) {
+        super(props)
+        this.wrapper = createRef()
+    }
+
     onMenuClick(e: any)
     {
         e.target.blur()
@@ -22,10 +29,19 @@ export default class MenuButton extends Component<MenuButtonProps>
         const { items, right, title = "Menu", children = null } = this.props;
 
         return (
-            <span className={ classList({ "menu-button": true, "menu-right": !!right }) }>
-                <button title={title} type="button">
+            <span
+                className={ classList({ "menu-button": true, "menu-right": !!right }) }
+                tabIndex={0}
+                ref={ this.wrapper }
+                onClick={e => {
+                    if (this.wrapper && this.wrapper.current && e.target !== this.wrapper.current) {
+                        this.wrapper.current.blur()
+                    }
+                }}
+            >
+                <div title={title} className="menu-button-btn">
                     { children || <i className="fa-solid fa-ellipsis-vertical default-icon"/> }
-                </button>
+                </div>
                 <div className="menu" onClick={this.onMenuClick}>
                     { items.filter(Boolean).map((item, i) => {
                         if (item === "separator") {
