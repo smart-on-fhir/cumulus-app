fc045bd818528bf55593142f09189544cc654491
Vladimir Ignatov <vlad.ignatov@gmail.com>
2022-05-28 09:03:59 -0400

    Improve migrations

--- backend/db/migrations/01-allow-html-in-request-description.js
+++ backend/db/migrations/01-allow-html-in-request-description.js
@@ -1,6 +1,11 @@
 const { DataTypes } = require('sequelize');
+const config        = require("../../config");
 
 async function up(queryInterface) {
+    if (config.db.sync === "force") {
+        return true;
+    }
+
     await queryInterface.changeColumn('DataRequests', 'description', {
         type: DataTypes.TEXT
     });
--- backend/db/migrations/02-add-rating-to-views.js
+++ backend/db/migrations/02-add-rating-to-views.js
@@ -1,6 +1,15 @@
 const { DataTypes } = require('sequelize');
+const config        = require("../../config");
 
 async function up(queryInterface) {
+
+    // We don't need to do anything if the DB is fully recreated
+    // From the model definitions, because the new columns are
+    // already defined in the model
+    if (config.db.sync === "force") {
+        return true;
+    }
+
     await queryInterface.addColumn('Views', 'rating', {
         type        : DataTypes.FLOAT,
         defaultValue: 0,
@@ -21,6 +30,7 @@ async function up(queryInterface) {
 async function down(queryInterface) {
 	await queryInterface.removeColumn('Views', 'rating');
     await queryInterface.removeColumn('Views', 'votes');
+    await queryInterface.removeColumn('Views', 'normalizedRating');
 }
 
 module.exports = { up, down };
