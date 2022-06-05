947d5c732cbdb8aacabc9fb08f1455f73ad0af40
Vladimir Ignatov <vlad.ignatov@gmail.com>
2022-06-04 20:11:18 -0400

    Remove select title

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -110,7 +110,7 @@ export default function Select({
             onKeyDown={ onKeyDown }
             onBlur={ closeMenu }
             onMouseDown={ toggleMenu }
-            title={ title || (selectedOption ? "Selected: " + JSON.stringify(selectedOption.value) : undefined) }
+            title={ title }
         >
             <div className="select-component-value">{
                 selectedOption ?
