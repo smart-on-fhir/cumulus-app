711c50325687aa0742c2d01c469c6238410c0944
Vladimir Ignatov <vlad.ignatov@gmail.com>
2022-07-10 17:02:18 -0400

    Add metadata coumn to requests

---
+++ backend/db/migrations/03-add-request-metadata.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require('sequelize');
+const config        = require("../../config");
+
+async function up(queryInterface) {
+
+    // We don't need to do anything if the DB is fully recreated
+    // From the model definitions, because the new columns are
+    // already defined in the model
+    if (config.db.sync === "force") {
+        return true;
+    }
+
+    await queryInterface.addColumn('DataRequests', 'metadata', {
+        type: DataTypes.JSONB
+    });
+}
+
+async function down(queryInterface) {
+	await queryInterface.removeColumn('DataRequests', 'metadata');
+}
+
+module.exports = { up, down };
