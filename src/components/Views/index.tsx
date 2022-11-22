b7cae2767698686a2892f8771ddaedd69f5484db
Vladimir Ignatov <vlad.ignatov@gmail.com>
2022-11-22 10:49:43 -0500

    Sort menu for graphs

--- src/components/Views/index.tsx
+++ src/components/Views/index.tsx
@@ -1,13 +1,21 @@
 import { useState } from "react";
 import { Helmet, HelmetProvider } from "react-helmet-async";
+import { classList } from "../../utils";
+import Checkbox from "../Checkbox";
+import MenuButton from "../MenuButton";
 import ViewsBrowser from "./ViewsBrowser";
+
+
+type SortType = "name-asc" | "name-desc" | "mod-asc" | "mod-desc" | "rating-asc" | "rating-desc" | ""
 
 export default function Views()
 {
     const url = new URL(window.location.href)
 
     const [ viewType, setViewType ] = useState(url.searchParams.get("view") || "grid")
-    const [ search, setSearch ] = useState(url.searchParams.get("q") || "")
+    const [ search  , setSearch   ] = useState(url.searchParams.get("q") || "")
+    const [ groupBy , setGroupBy  ] = useState(url.searchParams.get("group") || "")
+    const [ sort    , setSort     ] = useState<SortType>(String(url.searchParams.get("sort") || "") as SortType)
 
     const onSearch = (q: string) => {
         url.searchParams.set("q", q)
@@ -21,6 +29,26 @@ export default function Views()
         setViewType(t)
     };
 
+    const onSetGroupBy = (t: "subscription" | "tag" | "") => {
+        if (!t) {
+            url.searchParams.delete("group")
+        } else {
+            url.searchParams.set("group", t)
+        }
+        window.history.replaceState(null, "", url.href)
+        setGroupBy(t)
+    };
+
+    const onSetSort = (t: SortType) => {
+        if (!t) {
+            url.searchParams.delete("sort")
+        } else {
+            url.searchParams.set("sort", t)
+        }
+        window.history.replaceState(null, "", url.href)
+        setSort(t)
+    };
+
     return (
         <div>
             <HelmetProvider>
@@ -28,11 +56,14 @@ export default function Views()
                     <title>Cumulus Graphs</title>
                 </Helmet>
             </HelmetProvider>
-            <div className="row gap middle">
-                <div className="col col-0">
-                    <h4 className="m-0"><i className="icon fa-solid fa-chart-pie color-blue-dark" /> Graphs</h4>
+            <div className="row half-gap middle wrap">
+                <div className="col col-0 mb-05 nowrap">
+                    <h4 className="m-0">
+                        <i className="icon fa-solid fa-chart-pie color-blue-dark" /> Graphs
+                    </h4>
                 </div>
-                <div className="col center">
+                <div className="col col-0 responsive"/>
+                <div className="col col-3 mb-05 right responsive">
                     <input
                         type="search"
                         placeholder="Search Views by Name"
@@ -40,7 +71,62 @@ export default function Views()
                         onChange={e => onSearch(e.target.value)}
                     />
                 </div>
-                <div className="col col-0 right">
+                {/* <div className="col col-0 responsive"/> */}
+                <div className="col col-4 mb-05 nowrap responsive right">
+                    <div className="row">
+                        <MenuButton right title="Sort By" items={[
+                            <Checkbox onChange={() => onSetSort("name-asc"   )} checked={ sort === "name-asc"   } name="sort" type="radio" label={<>Sort by Name <span className="color-muted">(A-Z)</span></>} />,
+                            <Checkbox onChange={() => onSetSort("name-desc"  )} checked={ sort === "name-desc"  } name="sort" type="radio" label={<>Sort by Name <span className="color-muted">(Z-A)</span></>} />,
+                            "separator",
+                            <Checkbox onChange={() => onSetSort("mod-desc"   )} checked={ sort === "mod-desc"   } name="sort" type="radio" label={<>Sort by Modification <span className="color-muted">(newest first)</span></>} />,
+                            <Checkbox onChange={() => onSetSort("mod-asc"    )} checked={ sort === "mod-asc"    } name="sort" type="radio" label={<>Sort by Modification <span className="color-muted">(oldest first)</span></>} />,
+                            "separator",
+                            <Checkbox onChange={() => onSetSort("rating-desc")} checked={ sort === "rating-desc"} name="sort" type="radio" label={<>Sort by Rating <span className="color-muted">(highest first)</span></>} />,
+                            <Checkbox onChange={() => onSetSort("rating-asc" )} checked={ sort === "rating-asc" } name="sort" type="radio" label={<>Sort by Rating <span className="color-muted">(lowest first)</span></>} />,
+                            "separator",
+                            <Checkbox onChange={() => onSetSort("")} checked={ !sort } name="sort" type="radio" label={ <><b>Default Sort</b> <span className="color-muted">(rating, name, date)</span></> } />
+                        ]}>
+                            <i className={ classList({
+                                "fa-solid fa-sort": true,
+                                "color-muted": !sort,
+                                "color-brand-2": !!sort
+                             }) } /> Sort
+                            <span className="color-muted"> ▾</span>
+                        </MenuButton>
+                        <MenuButton right title="Group By" items={[
+                            <Checkbox
+                                onChange={() => onSetGroupBy("subscription")}
+                                checked={ groupBy === "subscription"}
+                                name="groupBy"
+                                type="radio"
+                                label="Group by Data Subscription"
+                            />,
+                            <Checkbox
+                                onChange={() => onSetGroupBy("tag")}
+                                checked={ groupBy === "tag"}
+                                name="groupBy"
+                                type="radio"
+                                label="Group by Tag"
+                            />,
+                            "separator",
+                            <Checkbox
+                                onChange={() => onSetGroupBy("")}
+                                checked={ !groupBy }
+                                name="groupBy"
+                                type="radio"
+                                label={ <b>No Grouping</b> }
+                            />
+                        ]}>
+                            <i className={ classList({
+                                "fa-solid fa-layer-group": true,
+                                "color-muted": !groupBy,
+                                "color-brand-2": !!groupBy
+                            })} /> Group
+                            <span className="color-muted"> ▾</span>                            
+                        </MenuButton>
+                    </div>
+                </div>
+                <div className="col col-0 mb-05">
                     <div className="toolbar flex">
                         <button
                             className={"btn" + (viewType === "grid" ? " active" : "")}
@@ -55,8 +141,12 @@ export default function Views()
                     </div>    
                 </div>
             </div>
-            <hr className="mt-1 mb-1"/>
-            <ViewsBrowser layout={ viewType === "grid" ? "grid" : "column" } search={search} />
+            <hr className="mt-05 mb-1"/>
+            <ViewsBrowser
+                layout={ viewType === "grid" ? "grid" : "column" }
+                search={search}
+                sort={sort}
+            />
         </div>
     )
 }
