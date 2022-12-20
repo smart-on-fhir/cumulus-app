a3f00f6efdc610732500d67006545b73d63e88bf
Vladimir Ignatov <vlad.ignatov@gmail.com>
2022-12-20 09:19:15 -0500

    UI permissions for tags #5

--- src/components/Tags/View.tsx
+++ src/components/Tags/View.tsx
@@ -1,4 +1,5 @@
 import { Link }           from "react-router-dom"
+import { useAuth }        from "../../auth"
 import DataRequestLink    from "../DataRequests/DataRequestLink"
 import { createViewPage } from "../generic/EndpointViewWrapper"
 import ViewThumbnail      from "../Views/ViewThumbnail"
@@ -6,12 +7,19 @@ import ViewThumbnail      from "../Views/ViewThumbnail"
 
 export default function View() {
 
+    const { user } = useAuth();
+
+    const canUpdate = user?.permissions.includes("Tags.update")
+    const canDelete = user?.permissions.includes("Tags.delete")
+
     return createViewPage<app.Tag>({
         endpoint    : "/api/tags",
         namePlural  : "Tags",
         icon        : <i className="fa-solid fa-tag color-brand-2" />,
         query       : "creator=true&graphs=true&subscriptions=true",
         basePath    : "/tags",
+        canUpdate,
+        canDelete,
         renderView  : data => <>
             <div className="mt-05">
                 <span className="color-muted">Created by: </span>
