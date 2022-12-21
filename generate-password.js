55f956a84895447a31bd2099f8d5e1d47199bcb8
Vladimir Ignatov <vlad.ignatov@gmail.com>
2022-12-21 10:44:08 -0500

    Clean up

--- generate-password.js
+++
@@ -1,7 +0,0 @@
-const bcrypt = require("bcryptjs")
-const { randomBytes } = require("crypto")
-
-const pass = randomBytes(16).toString("base64url")
-const hash = bcrypt.hashSync(pass, bcrypt.genSaltSync(10))
-
-console.log("pass: %o hash: %o", pass, hash)
