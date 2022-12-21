9c197bdf64947486ff4aacca5cf15b3b8d428ebf
Vladimir Ignatov <vlad.ignatov@gmail.com>
2022-12-21 09:38:13 -0500

    ACL for navigation #5

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -54,38 +54,39 @@ export default function Navigation()
         return null
     }
 
-    const canCreateProjects = user.permissions.includes("Projects.create")
+    const canReadSites         = user.permissions.includes("DataSites.read")
+    const canReadSubscriptions = user.permissions.includes("DataRequests.read")
+    const canReadGroups        = user.permissions.includes("RequestGroups.read")
+    const canReadTags          = user.permissions.includes("Tags.read")
+    const canReadGraphs        = user.permissions.includes("Views.read")
+    const canReadProjects      = user.permissions.includes("Projects.read")
+    const canCreateProjects    = user.permissions.includes("Projects.create")
+    const canReadUsers         = user.permissions.includes("Users.read")
+    const canReadActivities    = user.permissions.includes("Activities.read")
+    const canReadLogs          = user.permissions.includes("Logs.read")
 
     return (
         <div className="navigation">
             <div className="navigation-wrap">
                 <NavLink to="/"><i className="icon fa-solid fa-house-chimney"></i> Home</NavLink>
                 
-                
-                <NavGroup to="/projects" label="Study Areas" icon="fa-solid fa-book" end={ /^\/projects(\/\d+.*?)?$/ }>
-                    { canCreateProjects && <NavLink to="/projects/new"><i className="icon fa-solid fa-circle-plus" /> New Study Area</NavLink> }
-                </NavGroup>
+                { canReadProjects && (
+                    <NavGroup to="/projects" label="Study Areas" icon="fa-solid fa-book" end={ /^\/projects(\/\d+.*?)?$/ }>
+                        { canCreateProjects && <NavLink to="/projects/new"><i className="icon fa-solid fa-circle-plus" /> New Study Area</NavLink> }
+                    </NavGroup>
+                )}
 
-                <NavLink to="/views"><i className="icon fa-solid fa-chart-pie" /> Graphs</NavLink>
-
-                <NavLink to="/sites"><i className="icon fa-solid fa-location-dot" /> Data Sites</NavLink>
+                { canReadGraphs && <NavLink to="/views"><i className="icon fa-solid fa-chart-pie" /> Graphs</NavLink> }
+                { canReadSites && <NavLink to="/sites"><i className="icon fa-solid fa-location-dot" /> Data Sites</NavLink> }
+                { canReadSubscriptions && <NavLink to="/requests"><i className="icon fa-solid fa-database" /> Data Subscriptions</NavLink> }
+                { canReadGroups && <NavLink to="/groups"><i className="icon fa-solid fa-folder" /> Subscription Groups</NavLink> }
+                { canReadTags && <NavLink to="/tags"><i className="icon fa-solid fa-tag" /> Tags</NavLink> }
                 
-                <NavLink to="/requests"><i className="icon fa-solid fa-database" /> Data Subscriptions</NavLink>
-                <NavLink to="/groups"><i className="icon fa-solid fa-folder" /> Subscription Groups</NavLink>
-                {/* <NavGroup icon="fa-solid fa-database" label="Data">
-                    <NavLink to="/requests/new"><i className="icon fa-solid fa-calendar-plus" /> New Data Subscription</NavLink>
-                </NavGroup> */}
-                <NavLink to="/tags"><i className="icon fa-solid fa-tag" /> Tags</NavLink>
-                
-                { user.role === "admin" && (
+                { (canReadUsers || canReadLogs || canReadUsers) && (
                     <NavGroup icon="fa-solid fa-screwdriver-wrench" label="Administration">
-                        {/* <NavLink to="/settings"><i className="icon fa-solid fa-cog" /> Settings</NavLink> */}
-                        <NavLink to="/users" end><i className="icon fa-solid fa-users" /> Users</NavLink>
-                        {/* <NavLink to="/users/invite"><i className="icon fa-solid fa-user-plus" /> Invite User</NavLink> */}
-                        {/* <NavLink to="/permissions"><i className="icon fa-solid fa-user-shield" /> Roles &amp; Permissions</NavLink> */}
-                        <NavLink to="/activity"><i className="icon fa-solid fa-clipboard-list" /> Activity</NavLink>
-                        <NavLink to="/logs"><i className="icon fa-solid fa-clipboard-list" /> View Logs</NavLink>
-                        {/* <NavLink to="/tags"><i className="icon fa-solid fa-tag" /> Manage Tags</NavLink> */}
+                        { canReadUsers && <NavLink to="/users" end><i className="icon fa-solid fa-users" /> Users</NavLink> }
+                        { canReadActivities && <NavLink to="/activity"><i className="icon fa-solid fa-clipboard-list" /> Activity</NavLink> }
+                        { canReadLogs && <NavLink to="/logs"><i className="icon fa-solid fa-clipboard-list" /> View Logs</NavLink> }
                     </NavGroup>
                 )}
 
