c68fc76f7970961288063d733c6876edb2a5d2b9
Vladimir Ignatov <vlad.ignatov@gmail.com>
2022-12-21 11:02:28 -0500

    Convert RequestGroup model to TS

---
+++ backend/db/models/RequestGroup.ts
@@ -0,0 +1,42 @@
+import BaseModel from "./BaseModel"
+import {
+    Sequelize,
+    DataTypes,
+    InferAttributes,
+    InferCreationAttributes,
+    CreationOptional
+} from "sequelize"
+
+
+export default class RequestGroup extends BaseModel<InferAttributes<RequestGroup>, InferCreationAttributes<RequestGroup>>
+{
+    declare id: CreationOptional<number>;
+    declare name: string;
+    declare description: string;
+    declare createdAt: CreationOptional<Date>;
+    declare updatedAt: CreationOptional<Date>;
+
+    static initialize(sequelize: Sequelize) {
+        RequestGroup.init({
+            id: {
+                type         : DataTypes.INTEGER,
+                allowNull    : false,
+                primaryKey   : true,
+                autoIncrement: true
+            },
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: true
+            },
+            description: {
+                type: DataTypes.TEXT
+            },
+            createdAt: DataTypes.DATE,
+            updatedAt: DataTypes.DATE
+        }, {
+            sequelize,
+            modelName: "RequestGroup"
+        });
+    };
+}
