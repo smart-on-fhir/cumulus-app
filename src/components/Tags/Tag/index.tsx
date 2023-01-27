8a69cfe74d3c1d7c69227133d7f0a5ec687039dd
Vladimir Ignatov <vlad.ignatov@gmail.com>
2023-01-27 13:23:04 -0500

    Tags can also be links #11

--- src/components/Tags/Tag/index.tsx
+++ src/components/Tags/Tag/index.tsx
@@ -1,6 +1,9 @@
+import { Link } from "react-router-dom"
 import "./Tag.scss"
 
-export default function Tag({ tag }: { tag: Pick<app.Tag, "name" | "description">})
+export default function Tag({ tag, noLink }: { tag: Pick<app.Tag, "name" | "description" | "id">, noLink?: boolean })
 {
-    return <span className="tag" title={ tag.description || undefined }>{ tag.name }</span>
+    return noLink ?
+        <span className="tag" title={ tag.description || undefined }>{ tag.name }</span> :
+        <Link className="tag" title={ tag.description || undefined } to={`/tags/${tag.id}`}>{ tag.name }</Link>;
 }
