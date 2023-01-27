77f0afd144d352f9326002bedc9813dda54c7e36
Vladimir Ignatov <vlad.ignatov@gmail.com>
2023-01-27 14:05:05 -0500

    Ability to create subscription from group #15

--- src/components/RequestGroups/View.tsx
+++ src/components/RequestGroups/View.tsx
@@ -1,3 +1,4 @@
+import { Link }           from "react-router-dom"
 import { createViewPage } from "../generic/EndpointViewWrapper"
 import Clip               from "../generic/Clip"
 import Grid               from "../generic/Grid"
@@ -9,8 +10,9 @@ export default function RequestGroupView()
 {
     const { user } = useAuth();
 
-    const canUpdate = user?.permissions.includes("RequestGroups.update")
-    const canDelete = user?.permissions.includes("RequestGroups.delete")
+    const canUpdate    = user?.permissions.includes("RequestGroups.update")
+    const canDelete    = user?.permissions.includes("RequestGroups.delete")
+    const canCreateSub = user?.permissions.includes("DataRequests.create")
 
     return createViewPage<app.RequestGroup>({
         basePath: "/groups",
@@ -30,20 +32,28 @@ export default function RequestGroupView()
             </div>
             <div className="mt-2 row gap wrap">
                 <div className="col col-4 responsive">
-                    { data.requests && data.requests.length ?
-                        <>
+                    <div className="row gap mt-2">
+                        <div className="col col-0 top responsive">
                             <h6 className="color-brand-2">
                                 <i className="fa-solid fa-link color-brand-2" /> Associated Subscriptions
                             </h6>
-                            <hr/>
-                            <Grid gap="0 1rem" cols="22em" className="link-list mt-05">{
-                                data.requests?.map((s, i) => (
-                                    <DataRequestLink key={i} request={s} href={"/requests/" + s.id} />
-                                ))
-                            }</Grid>
-                        </> :
-                        <div>
-                            <hr className="mb-1"/>
+                        </div>
+                        { canCreateSub && <div className="col col-0 right nowrap middle">
+                            <div>
+                                <Link to={`/requests/new?groupId=${data.id}`} className="btn btn-virtual" title="Create new subscription in this group">
+                                    <i className="fa-solid fa-plus-circle color-blue" />
+                                </Link>
+                            </div>
+                        </div> }
+                    </div>
+                    <hr/>
+                    { data.requests && data.requests.length ?
+                        <Grid gap="0 1rem" cols="22em" className="link-list mt-05">{
+                            data.requests?.map((s, i) => (
+                                <DataRequestLink key={i} request={s} href={"/requests/" + s.id} />
+                            ))
+                        }</Grid> :
+                        <div className="mt-1">
                             <span className="material-icons-round color-blue">
                                 info_outline
                             </span> No subscriptions are associated with this group.
