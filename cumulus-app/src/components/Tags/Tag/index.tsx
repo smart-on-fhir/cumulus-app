18cfe4fd7e512c4e8f05b2ef24480833031fb784
Matt Garber <matthew.garber@childrens.harvard.edu>
2023-05-15 13:52:37 -0400

    remove duplicate files

--- cumulus-app/src/components/Tags/Tag/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Link } from "react-router-dom"
-import "./Tag.scss"
-
-export default function Tag({ tag, noLink }: { tag: Pick<app.Tag, "name" | "description" | "id">, noLink?: boolean })
-{
-    return noLink ?
-        <span className="tag" title={ tag.description || undefined }>{ tag.name }</span> :
-        <Link className="tag" title={ tag.description || undefined } to={`/tags/${tag.id}`}>{ tag.name }</Link>;
-}
--- cumulus-app/src/components/generic/Loader/index.tsx
+++
@@ -1,5 +0,0 @@
-export default function Loader({ msg = "Loading..." }: { msg?: string }) {
-    return (
-        <span><i className="fas fa-circle-notch fa-spin"/> {msg}</span>
-    )
-}
