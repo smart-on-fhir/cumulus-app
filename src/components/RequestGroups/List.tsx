e078446ed85ba99f6393b973953a1ea9867382bc
Vladimir Ignatov <vlad.ignatov@gmail.com>
2023-06-16 08:42:31 -0400

    Empty state for request groups

--- src/components/RequestGroups/List.tsx
+++ src/components/RequestGroups/List.tsx
@@ -9,49 +9,63 @@ export default function RequestGroupList()
 {
     const { user } = useAuth();
 
+    const canCreate = user?.permissions.includes("RequestGroups.create")
+
     return createListPage<app.RequestGroup[]>({
         nameSingular: "Subscription Group",
         namePlural  : "Subscription Groups",
         endpoint    : "/api/request-groups",
         icon        : <i className="fa-solid fa-folder color-brand-2" />,
-        canCreate   : user?.permissions.includes("RequestGroups.create"),
-        renderList  : data => <>
-            <div className="mb-2">
-                <Alert color="blue" icon="fa-solid fa-info-circle">
-                    Subscriptions can be assigned to certain group, otherwise
-                    they are considered part of the <b>GENERAL</b> group.
-                </Alert>
-            </div>
-            <table className="endpoint-list-table">
-                <thead>
-                    <tr>
-                        <th style={{ width: "2em" }}>ID</th>
-                        <th style={{ width: "60%" }}>Name</th>
-                        <th>Description</th>
-                        <IfAllowed showError={false} permissions="RequestGroups.update" element={ <th style={{ width: "2.5em" }} /> } />
-                        <IfAllowed showError={false} permissions="RequestGroups.delete" element={ <th style={{ width: "2.5em" }} /> } />
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.filter((g: any) => !!g.id).map((row, i) => (
-                        <tr key={i}>
-                            <td>{row.id}</td>
-                            <td><Link title={row.name} to={"./" + row.id} className="link">{row.name}</Link></td>
-                            <td>{row.description || ""}</td>
-                            <IfAllowed showError={false} permissions="RequestGroups.update" element={ <td className="right nowrap">
-                                <Link title="Edit" className="btn small color-brand-2 btn-virtual" to={ row.id + "/edit" }>
-                                    <i className="fa-solid fa-pen-to-square" />
-                                </Link>
-                            </td> } />
-                            <IfAllowed showError={false} permissions="RequestGroups.delete" element={ <td className="right nowrap">
-                                <Link title="Delete" className="btn small color-red btn-virtual" to={ row.id + "/delete" }>
-                                    <i className="fa-solid fa-trash-can" />
-                                </Link>
-                            </td> } />
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </>
+        canCreate,
+        renderList  : data => {
+            data = data.filter((g: any) => !!g.id)
+            return (
+                <>
+                    <div className="mb-2">
+                        <Alert color="blue" icon="fa-solid fa-info-circle">
+                            Subscriptions can be assigned to certain group, otherwise
+                            they are considered part of the <b>GENERAL</b> group.
+                        </Alert>
+                    </div>
+                    
+                    { data.length ?
+                        <table className="endpoint-list-table">
+                            <thead>
+                                <tr>
+                                    <th style={{ width: "2em" }}>ID</th>
+                                    <th style={{ width: "60%" }}>Name</th>
+                                    <th>Description</th>
+                                    <IfAllowed showError={false} permissions="RequestGroups.update" element={ <th style={{ width: "2.5em" }} /> } />
+                                    <IfAllowed showError={false} permissions="RequestGroups.delete" element={ <th style={{ width: "2.5em" }} /> } />
+                                </tr>
+                            </thead>
+                            <tbody>
+                                { data.map((row, i) => (
+                                    <tr key={i}>
+                                        <td>{row.id}</td>
+                                        <td><Link title={row.name} to={"./" + row.id} className="link">{row.name}</Link></td>
+                                        <td>{row.description || ""}</td>
+                                        <IfAllowed showError={false} permissions="RequestGroups.update" element={ <td className="right nowrap">
+                                            <Link title="Edit" className="btn small color-brand-2 btn-virtual" to={ row.id + "/edit" }>
+                                                <i className="fa-solid fa-pen-to-square" />
+                                            </Link>
+                                        </td> } />
+                                        <IfAllowed showError={false} permissions="RequestGroups.delete" element={ <td className="right nowrap">
+                                            <Link title="Delete" className="btn small color-red btn-virtual" to={ row.id + "/delete" }>
+                                                <i className="fa-solid fa-trash-can" />
+                                            </Link>
+                                        </td> } />
+                                    </tr>
+                                )) }
+                            </tbody>
+                        </table> :
+                        <div className="center color-grey-dark bold">
+                            No subscription groups found in the database.
+                            { canCreate && <span> You can start by <Link to="new" className="link">creating one</Link>.</span> }
+                        </div>
+                    }   
+                </>
+            )
+        }
     })
 }
