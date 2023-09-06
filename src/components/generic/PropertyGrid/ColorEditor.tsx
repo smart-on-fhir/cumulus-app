7a35ed1f28b8d702855e60ad2d22d0277ab06fe7
Vladimir Ignatov <vlad.ignatov@gmail.com>
2023-09-06 06:37:05 -0400

    Enable short hex colors in property grid

--- src/components/generic/PropertyGrid/ColorEditor.tsx
+++ src/components/generic/PropertyGrid/ColorEditor.tsx
@@ -2,11 +2,21 @@ import { EditableProperty } from "./types"
 
 
 export default function ColorEditor({ prop }: { prop: EditableProperty }) {
+
+    let c = String(prop.value || "#000000")
+
+    if (c.match(/^#[0-9A-F]{3}$/i)) {
+        c = "#" + c[1] + c[1] + c[2] + c[2] + c[3] + c[3]
+    }
+    else if (c.match(/^#[0-9A-F]{8}$/i)) {
+        c = c.substring(0, 7)
+    }
+
     return (
         <span style={{ display: "flex" }}>
             <input
                 type="color"
-                value={ prop.value || "#000000" }
+                value={ c }
                 onChange={ e => prop.onChange(e.target.value) }
                 className={ prop.value ? undefined : "empty" }
                 disabled={!!prop.disabled}
