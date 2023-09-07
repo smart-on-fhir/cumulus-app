8163a502f36da4e8436a01c2fbecef21ac4954c2
Vladimir Ignatov <vlad.ignatov@gmail.com>
2023-09-07 16:37:05 -0400

    Improved color pickers

--- src/components/generic/PropertyGrid/ColorEditor.tsx
+++ src/components/generic/PropertyGrid/ColorEditor.tsx
@@ -1,16 +1,22 @@
 import { EditableProperty } from "./types"
 
+function parse(s: any) {
+    s = String(s || "").trim()
+    if (s.startsWith("#")) {
+        if (s.length === 4) { // #RGB
+            return `#${s[1]}${s[1]}${s[2]}${s[2]}${s[3]}${s[3]}`
+        }
+        else if (s.length === 5) { // #RGBA
+            return `#${s[1]}${s[1]}${s[2]}${s[2]}${s[3]}${s[3]}${s[4]}${s[4]}`
+        }
+    }    
+    return s
+}
+
 
 export default function ColorEditor({ prop }: { prop: EditableProperty }) {
 
-    let c = String(prop.value || "#000000")
-
-    if (c.match(/^#[0-9A-F]{3}$/i)) {
-        c = "#" + c[1] + c[1] + c[2] + c[2] + c[3] + c[3]
-    }
-    else if (c.match(/^#[0-9A-F]{8}$/i)) {
-        c = c.substring(0, 7)
-    }
+    let c = parse(prop.value)
 
     return (
         <span style={{ display: "flex" }}>
@@ -20,11 +26,13 @@ export default function ColorEditor({ prop }: { prop: EditableProperty }) {
                 onChange={ e => prop.onChange(e.target.value) }
                 className={ prop.value ? undefined : "empty" }
                 disabled={!!prop.disabled}
+                style={{ background: `linear-gradient(${c}, ${c}), repeating-linear-gradient(-45deg, #FFF, #CCC 2px, #FFF 3px, #FFF 4px)` }}
             />
             <input
                 type="text"
                 value={ prop.value || "" }
                 onChange={ e => prop.onChange(e.target.value) }
+                spellCheck={false}
             />
         </span>
     )
