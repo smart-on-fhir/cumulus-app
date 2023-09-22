0978247235ef1e3f9f4ac901b6ff147a6031f10f
Vladimir Ignatov <vlad.ignatov@gmail.com>
2023-09-22 13:26:47 -0400

    New Toggle component

---
+++ src/components/generic/Toggle/index.tsx
@@ -0,0 +1,46 @@
+import { KeyboardEvent } from "react"
+import { classList }     from "../../../utils"
+import "./Toggle.scss"
+
+
+export default function Toggle({
+    checked,
+    onChange,
+    className,
+    disabled
+}: {
+    checked   : boolean
+    onChange  : (checked: boolean) => void
+    className?: string
+    disabled ?: boolean
+}) {
+    
+    function onkeydown(e: KeyboardEvent) {
+        if (e.key === "Enter" || e.key === " ") {
+            e.preventDefault()
+            onChange(!checked)
+        }
+        if (e.key === "ArrowLeft") {
+            e.preventDefault()
+            onChange(false)
+        }
+        if (e.key === "ArrowRight") {
+            e.preventDefault()
+            onChange(true)
+        }
+    }
+
+    return (
+        <b
+            className={ classList({
+                "toggle": true,
+                [className || ""]: !!className,
+                "grey-out": !!disabled,
+                "on": !!checked
+            })}
+            tabIndex={0}
+            onKeyDown={onkeydown}
+            onClick={() => onChange(!checked)}
+        />
+    )
+}
