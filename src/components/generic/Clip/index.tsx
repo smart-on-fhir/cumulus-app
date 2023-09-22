bea5077f769ce66bbe06b73e2719ca58773a142f
Vladimir Ignatov <vlad.ignatov@gmail.com>
2023-09-22 12:51:12 -0400

    Stop click event propagation in Clip component links

--- src/components/generic/Clip/index.tsx
+++ src/components/generic/Clip/index.tsx
@@ -13,7 +13,11 @@ export default function Clip({ txt = "", max = 200 }: { txt?: string, max?: numb
         <span className="clip">
             <span>{ expanded ? txt : txt.substring(0, max) + "..." }</span>
             <b className="text-info small ml-05" style={{ cursor: "pointer", whiteSpace: "nowrap" }}
-                onClick={() => setExpanded(!expanded)}>
+                onClick={e => {
+                    e.stopPropagation()
+                    e.preventDefault()
+                    setExpanded(!expanded)
+                }}>
                 <i className="link"> { expanded ? "Show less": "Show more" }</i>
             </b>
         </span>
