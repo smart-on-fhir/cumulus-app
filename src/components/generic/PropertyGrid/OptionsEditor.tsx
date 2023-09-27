358a7577b1ef200e47a057411c3330483b02a90f
Vladimir Ignatov <vlad.ignatov@gmail.com>
2023-09-27 15:25:12 -0400

    OptionsEditor can render opt-groups

--- src/components/generic/PropertyGrid/OptionsEditor.tsx
+++ src/components/generic/PropertyGrid/OptionsEditor.tsx
@@ -8,8 +8,12 @@ export interface EditableOptionsProperty extends EditableProperty {
     disabled?: boolean
     title?: string
     style?: CSSProperties
-    options?: (string | { value: string, label: string })[]
+    options?: (Option | OptGroup)[]
 }
+
+export type Option = string | OptionObject
+export type OptionObject = { value: string, label: string, disabled?: boolean }
+export type OptGroup = { value: Option[], label: string, disabled?: boolean }
 
 export default function OptionsEditor({ prop }: { prop: EditableOptionsProperty }) {
     const { value, required, onChange, disabled, title, style, options = [] } = prop
@@ -23,12 +27,22 @@ export default function OptionsEditor({ prop }: { prop: EditableOptionsProperty 
             title={ title }
             style={ style }
         >
-            { options.map((o, i) => {
-                if (typeof o === "string") {
-                    return <option key={i} value={o}>{o}</option>
-                }
-                return <option key={i} value={o.value}>{o.label}</option>
-            }) }
+            <Options options={options} />
         </select>
     )
+}
+
+function Options({ options }: { options: (Option | OptGroup)[] }) {
+    return <>{ options.map((o, i) => {
+        if (typeof o === "string") {
+            return <option key={i} value={o}>{o}</option>
+        }
+        if (Array.isArray(o.value)) {
+            return <optgroup key={i} label={o.label} disabled={o.disabled}>
+                <Options options={o.value} />
+            </optgroup>
+        }
+        return <option key={i} value={o.value} disabled={o.disabled}>{o.label}</option>
+    }) }
+    </>
 }
