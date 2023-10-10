f0ece36f5fb289df9f4c99d7073d3a6caf8b0812
Vladimir Ignatov <vlad.ignatov@gmail.com>
2023-10-10 11:59:46 -0400

    Enable 3D option edits #52

--- src/components/Dashboard/Charts/Pie.ts
+++ src/components/Dashboard/Charts/Pie.ts
@@ -4,7 +4,13 @@ const options: Options = {
     chart: {
         type: "pie",
         options3d: {
-            enabled: false
+            enabled: false,
+            alpha: 45,
+            beta: 0,
+            depth: 350,
+            fitToPlot: true,
+            viewDistance: 25,
+            axisLabelPosition: null
         }
     },
     plotOptions: {
--- src/components/Dashboard/Inspectors/Chart.tsx
+++ src/components/Dashboard/Inspectors/Chart.tsx
@@ -11,67 +11,85 @@ export default function Chart({
     onChange: (a: Partial<ChartOptions>) => void
 }) {
 
-    const props = getEditorPropsFromSchemaPaths(
-        { chart },
-        x => onChange(x.chart),
-        [
-            "chart.backgroundColor",
-            // "chart.borderColor",
-            // "chart.borderWidth",
-            // "chart.borderRadius",
-            // "chart.polar",
-            {
-                name: "Spacing",
-                type: "group",
-                open: true,
-                value: [
-                    "chart.spacingTop",
-                    "chart.spacingRight",
-                    "chart.spacingBottom",
-                    "chart.spacingLeft",
-                ]
-            },
-            {
-                name: "Margin",
-                type: "group",
-                open: true,
-                value: [
-                    "chart.marginTop",
-                    "chart.marginRight",
-                    "chart.marginBottom",
-                    "chart.marginLeft",
-                ]
-            },
-            {
-                name: "Style",
-                type: "group",
-                open: true,
-                description: <div>
-                    <i className="fas fa-info-circle color-blue"/> Objects
-                    in the chart will use this font family, unless configured
-                    otherwise. They will also use font sizes relative to this
-                    one.
-                </div>,
-                value: [
-                    "chart.style.fontSize",
-                    "chart.style.fontFamily",
-                ]
-            },
-            // {
-            //     name: "3D Options",
-            //     type: "group",
-            //     value: [
-            //         "chart.options3d.enabled",
-            //         "chart.options3d.alpha",
-            //         "chart.options3d.beta",
-            //         "chart.options3d.depth",
-            //         "chart.options3d.viewDistance",
-            //         "chart.options3d.fitToPlot",
-            //         "chart.options3d.axisLabelPosition"
-            //     ]
-            // }
-        ]
-    )
+    const paths: any[] = [
+        "chart.backgroundColor",
+        // "chart.borderColor",
+        // "chart.borderWidth",
+        // "chart.borderRadius",
+        // "chart.polar",
+        {
+            name: "Spacing",
+            type: "group",
+            open: true,
+            value: [
+                "chart.spacingTop",
+                "chart.spacingRight",
+                "chart.spacingBottom",
+                "chart.spacingLeft",
+            ]
+        },
+        {
+            name: "Margin",
+            type: "group",
+            open: true,
+            value: [
+                "chart.marginTop",
+                "chart.marginRight",
+                "chart.marginBottom",
+                "chart.marginLeft",
+            ]
+        },
+        {
+            name: "Style",
+            type: "group",
+            open: true,
+            description: <div>
+                <i className="fas fa-info-circle color-blue"/> Objects
+                in the chart will use this font family, unless configured
+                otherwise. They will also use font sizes relative to this
+                one.
+            </div>,
+            value: [
+                "chart.style.fontSize",
+                "chart.style.fontFamily",
+            ]
+        }
+    ];
+
+    const options3d = []
+    if (chart.type === "pie") {
+        options3d.push("chart.options3d.enabled", {
+            name: "Alpha",
+            type: "number",
+            description: "One of the two rotation angles for the chart.",
+            min: 0,
+            max: 90,
+            defaultValue: 45,
+            value: chart.options3d?.alpha ?? 0,
+            onChange: (alpha?: number) => onChange({ options3d: { alpha }})
+        })
+    }
+    else {
+        options3d.push(
+            "chart.options3d.enabled",
+            "chart.options3d.alpha",
+            "chart.options3d.beta",
+            "chart.options3d.depth",
+            "chart.options3d.viewDistance",
+            "chart.options3d.fitToPlot",
+            "chart.options3d.axisLabelPosition",
+            "chart.options3d.frame.visible"
+        )
+    }
+
+    paths.push({
+        name: "3D Options",
+        type: "group",
+        open: true,
+        value: options3d
+    })
+
+    const props = getEditorPropsFromSchemaPaths({ chart }, x => onChange(x.chart), paths)
 
     return <PropertyGrid props={props} />
 }
