8ce82c1ef98b8e23928ff58090dc5440c5ed4edd
Vladimir Ignatov <vlad.ignatov@gmail.com>
2023-11-10 12:08:37 -0500

    New seeds for tests #54

--- test/fixtures/Views.ts
+++ test/fixtures/Views.ts
@@ -7,5 +7,24 @@ export default [
         createdAt: '2022-06-06 21: 06: 06.518+00',
         updatedAt: '2022-11-15 16: 27: 59.598+00',
         DataRequestId: 1
+    },
+    {
+        id: 2,
+        name: 'Graph owned by User#3',
+        description: '',
+        settings: {},
+        createdAt: '2022-06-06 21: 06: 06.518+00',
+        updatedAt: '2022-11-15 16: 27: 59.598+00',
+        DataRequestId: 1,
+        owner_id: 3
+    },
+    {
+        id: 3,
+        name: 'Graph shared with User#3',
+        description: '',
+        settings: {},
+        createdAt: '2022-06-06 21: 06: 06.518+00',
+        updatedAt: '2022-11-15 16: 27: 59.598+00',
+        DataRequestId: 1
     }
 ]
