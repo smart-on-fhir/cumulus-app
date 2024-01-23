338ed4f252a8717f004b28aa62b58486be2deb65
Vladimir Ignatov <vlad.ignatov@gmail.com>
2024-01-23 10:51:09 -0500

    More style editors for legend items

--- src/components/Dashboard/Inspectors/Legend.tsx
+++ src/components/Dashboard/Inspectors/Legend.tsx
@@ -2,6 +2,8 @@ import { LegendOptions } from "highcharts"
 import PropertyGrid from "../../generic/PropertyGrid"
 import { getEditorPropsFromSchemaPaths } from "../Schema"
 import { DEFS } from "../Schema/definitions";
+import { lengthToEm } from "../../../utils";
+import { DEFAULT_FONT_FAMILY } from "../config";
 
 
 export default function Legend({
@@ -54,6 +56,26 @@ export default function Legend({
                         ...DEFS.fontWeight,
                         value: legend.itemStyle?.fontWeight ?? "700",
                         onChange: (fontWeight?: string) => onChange({ itemStyle: { ...legend.itemStyle, fontWeight }})
+                    } as any,
+                    {
+                        ...DEFS.fontSize,
+                        value: lengthToEm(legend.itemStyle?.fontSize ?? "0.75em"),
+                        onChange: (fontSize?: number) => onChange({ itemStyle: { ...legend.itemStyle, fontSize: fontSize + "em" }})
+                    } as any,
+                    {
+                        ...DEFS.fontFamily,
+                        value: legend.itemStyle?.fontFamily ?? DEFAULT_FONT_FAMILY,
+                        onChange: (fontFamily: string) => onChange({ itemStyle: { ...legend.itemStyle, fontFamily }})
+                    } as any,
+                    {
+                        ...DEFS.whiteSpace,
+                        value: legend.itemStyle?.whiteSpace ?? "normal",
+                        onChange: (whiteSpace: string) => onChange({ itemStyle: { ...legend.itemStyle, whiteSpace }})
+                    } as any,
+                    {
+                        ...DEFS.textOverflow,
+                        value: legend.itemStyle?.textOverflow,
+                        onChange: (textOverflow: string) => onChange({ itemStyle: { ...legend.itemStyle, textOverflow }})
                     } as any
                 ]
             },
