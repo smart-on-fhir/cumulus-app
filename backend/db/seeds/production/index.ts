7347c4686e792c407674f62b4e8cc83148d77ad8
Vladimir Ignatov <vlad.ignatov@gmail.com>
2024-01-25 16:19:55 -0500

    Add permissions to production seeds

--- backend/db/seeds/production/index.ts
+++ backend/db/seeds/production/index.ts
@@ -1,10 +1,12 @@
 import { Sequelize } from "sequelize"
 import users         from "./users"
 import tags          from "./tags"
+import permissions   from "./permissions"
 import { seedTable } from "../lib"
 
 
 export async function seed(connection: Sequelize) {
     await seedTable(connection, "User", users)
     await seedTable(connection, "Tag", tags)
+    await seedTable(connection, "Permission", permissions)
 }
