966702185a21a8c3022376fa6d471493c8f400c5
Vladimir Ignatov <vlad.ignatov@gmail.com>
2024-02-09 17:12:22 -0500

    Support TSV parsing

--- backend/DataManager/Line2CSV.ts
+++ backend/DataManager/Line2CSV.ts
@@ -3,19 +3,25 @@ import { parseDelimitedLine } from "../lib"
 
 
 export default class Line2CSV extends Transform
-{    
-    constructor()
+{
+    delimiters: string[];
+
+    quotes: string;
+
+    constructor(delimiters = [","], quotes = '"')
     {
         super({
             writableObjectMode: true,
             readableObjectMode: true
         });
+        this.delimiters = delimiters
+        this.quotes = quotes
     }
 
     _transform(line: string, _encoding: any, next: any)
     {
         try {
-            let data = parseDelimitedLine(line);
+            let data = parseDelimitedLine(line, this.delimiters, this.quotes);
             next(null, data);
         } catch (ex) {
             next(ex);
