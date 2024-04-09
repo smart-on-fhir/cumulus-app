baf068b61af8ec262245cd648c80b498b9ccfb6e
Vladimir Ignatov <vlad.ignatov@gmail.com>
2024-04-09 12:19:56 -0400

    Remove unused components #68

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,4 +1,3 @@
-import moment from "moment";
 import { app } from "./types"
 
 
@@ -42,124 +41,11 @@ export function classList(map: Record<string, boolean>): string | undefined {
     return cls.join(" ") || undefined
 }
 
-interface FormatOptions {
-    skipNull?: boolean
-    skipUndefined?: boolean
-    maxListLength?: number
-    maxLength?: number
-    html?: boolean
-}
 
-export function format(
-    value: any|any[],
-    dataType: app.supportedDataType,
-    options: FormatOptions = {}
-): string {
-    if (Array.isArray(value)) {
-        return formatArray(value, dataType, options)
-    }
 
-    const { html, skipNull, skipUndefined, maxLength = 0 } = options
 
-    if (value === null) {
-        return skipNull ? "" : html ? '<span class="null">null</span>' : "null"
-    }
 
-    if (value === undefined) {
-        return skipUndefined ? "" : html ? '<span class="undefined">undefined</span>' : "undefined"
-    }
 
-    let formatted = "";
-
-    switch (dataType) {
-        case "boolean":
-            formatted = String(value === true)
-            break;
-        case "integer":
-            formatted = String(parseInt(value + "", 10))
-            break;
-        case "float":
-            formatted = String(parseFloat(value + ""))
-            break;
-        case "date:YYYY":
-            formatted = moment(value).format("YYYY")
-            break;
-        case "date:YYYY-MM":
-            formatted = moment(value).format("YYYY-MM")
-            break;
-        case "date:YYYY-MM-DD":
-            formatted = moment(value).format("YYYY-MM-DD")
-            break;
-        case "string":
-            formatted = value + ""
-            break;
-        default:
-            throw new Error(`Unsupported format for data type "${dataType}"`)
-    }
-
-    if (maxLength > 0 && maxLength < formatted.length) {
-        formatted = formatted.substr(0, maxLength) + "…"
-    }
-
-    if (html) {
-        formatted = `<span class="${dataType}">${formatted}</span>`
-    }
-
-    return formatted
-}
-
-export function formatArray(
-    value: any[],
-    dataType: app.supportedDataType,
-    options: FormatOptions = {}
-): string
-{
-    const {
-        maxListLength = 0,
-        maxLength = 0,
-        skipNull,
-        skipUndefined
-    } = options;
-
-    let overflowed = false
-    
-    let list: string[] = [], len = 0, strLen = 0
-
-    for (const cur of value) {
-
-        if (skipNull && cur === null) {
-            continue
-        }
-    
-        if (skipUndefined && cur === undefined) {
-            continue
-        }
-
-        // Overflow by number of list items
-        if (maxListLength > 0 && len >= maxListLength) {
-            overflowed = true;
-            break
-        }
-
-        const formatted = format(cur, dataType, options)
-
-        // Overflow by total string length
-        if (maxLength > 0) {
-            strLen += formatted.replace(/<.*?>/g, "").length
-            if (strLen >= maxLength) {
-                overflowed = true;
-                break
-            }
-        }
-
-        len = list.push(formatted)
-    }
-
-    return list.join(", ") + (overflowed ?
-        ", … " + (value.length - list.length) + " more" :
-        ""
-    )
-}
 
 const dictionaryMap = new Map([
     [/\bicd(\s+|-)?10\b/gi     , "ICD10"    ],
