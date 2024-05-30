53d4e7df7c573920031f345d6ab42437e86d741d
Vladimir Ignatov <vlad.ignatov@gmail.com>
2024-05-30 09:27:13 -0400

    Color themes #55, #75

--- src/components/Dashboard/config.ts
+++ src/components/Dashboard/config.ts
@@ -67,16 +67,17 @@ export const DASH_STYLES: DashStyleValue[] = [
 ];
 
 export const COLOR_THEMES = [
+    { id: "cumulus"      , name: "Cumulus Default", colors: generateColors(12, 70, 55, 0.46, 220) },
+    { id: "cumulus_muted", name: "Cumulus Muted"  , colors: generateColors(12, 40, 70, 0.46, 220) },
+    { id: "cumulus_light", name: "Cumulus Light"  , colors: generateColors(12, 75, 75, 0.46, 220) },
+    { id: "cumulus_dark" , name: "Cumulus Dark"   , colors: generateColors(12, 75, 35, 0.46, 220) },
     { id: "sas_dark"     , name: "SAS Line Colors", colors: ["#445694","#A23A2E","#01665E","#543005","#9D3CDB","#7F8E1F","#2597FA","#B26084","#D17800","#47A82A","#B38EF3","#F9DA04"] },
     { id: "sas_light"    , name: "SAS Fill Colors", colors: ["#6F7EB3","#D05B5B","#66A5A0","#A9865B","#B689CD","#BABC5C","#94BDE1","#CD7BA1","#CF974B","#87C873","#B7AEF1","#DDD17E"] },
-    { id: "cumulus_light", name: "Cumulus Light"  , colors: generateColors(12, 75, 75, 0.46, 220) },
-    { id: "cumulus_muted", name: "Cumulus Muted"  , colors: generateColors(12, 40, 70, 0.46, 220) },
-    { id: "cumulus"      , name: "Cumulus Default", colors: generateColors(12, 70, 55, 0.46, 220) },
-    { id: "cumulus_dark" , name: "Cumulus Dark"   , colors: generateColors(12, 75, 35, 0.46, 220) },
     { id: "smart"        , name: "SMART"          , colors: ["#207ccd","#865fda","#bd6bc2","#e44688","#df9320","#c4ae21","#64c10d","#26c7bd","#47bbf4","#97b9d4","#7a76ad","#cabe70"] },
     { id: "smart2"       , name: "SMART 2"        , colors: ["#739df2","#a383e2","#b87ab8","#d65c5c","#df9f20","#dfdf11","#66c20a","#53ac71","#77c5c5","#94b3d1","#8787c5","#ccbb66"] },
     { id: "gradient-1"   , name: "Gradient 1"     , colors: ["#75d7d7","#75a6d7","#7575d7","#a675d7","#d775d7","#d775a6","#d77575","#d7a675","#d7d775","#a6d775","#75d775","#75d7a6"] },
-    { id: "gradient-2"   , name: "Gradient 2"     , colors: ["#d77575","#d7a675","#d7d775","#a6d775","#75d775","#75d7a6","#75d7d7","#75a6d7","#7575d7","#a675d7","#d775d7","#d775a6"] }
+    { id: "gradient-2"   , name: "Gradient 2"     , colors: ["#d775d7","#d775a6","#d77575","#d7a675","#d7d775","#a6d775","#75d775","#75d7a6","#75d7d7","#75a6d7","#7575d7","#a675d7"] },
+    { id: "gradient-3"   , name: "Gradient 3"     , colors: ["#d7d775","#a6d775","#75d775","#75d7a6","#75d7d7","#75a6d7","#7575d7","#a675d7","#d775d7","#d775a6","#d77575","#d7a675"] },
 ]
 
 export const DEFAULT_COLOR_THEME = "cumulus"
