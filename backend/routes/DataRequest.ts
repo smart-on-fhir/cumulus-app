4fe2732a79d299469e66149308dc245f6c342ad8
Vladimir Ignatov <vlad.ignatov@gmail.com>
2024-06-03 15:50:06 -0400

    Fixes #57

--- backend/routes/DataRequest.ts
+++ backend/routes/DataRequest.ts
@@ -473,13 +473,11 @@ router.get("/:id/api", rw(async (req: AppRequest, res: Response) => {
         
         sql += `, sum(cnt::float) AS total FROM "${table}"`
 
-        let where: string[] = [ `"${stratifier}" IS NULL` ]
+        let where: string[] = []
 
         if (unusedColumns.length) {
             where.push(...unusedColumns.map(c => `"${c.name}" IS NULL`))
         }
-
-        // TODO: issue if stratifier is also in filters...
         
         if (filterWhere) {
             where.push(`(${ filterWhere })`)
