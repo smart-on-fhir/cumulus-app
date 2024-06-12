487bdae856f595d95b537b1e549428b1a023ba53
Vladimir Ignatov <vlad.ignatov@gmail.com>
2024-06-12 09:19:45 -0400

    Add GraphToolbar component

---
+++ src/components/Dashboard/GraphToolbar.tsx
@@ -0,0 +1,96 @@
+import { useAuth } from "../../auth"
+import CommandButton from "../../commands/CommandButton"
+import { app } from "../../types"
+import { requestPermission } from "../../utils"
+import { DashboardState, ViewAction } from "../Dashboard2/types"
+import { SaveChartButton } from "./SaveChartButton"
+
+export function GraphToolbar({
+    state,
+    dispatch,
+    graph,
+    save,
+    saving,
+    copyCommand,
+    shareCommand,
+    deleteCommand
+}: {
+    state: Pick<DashboardState, "inspection"|"viewType"|"showOptions">
+    dispatch: (action: ViewAction) => void
+    graph: Partial<app.View>
+    save: () => Promise<void>
+    saving: boolean
+    copyCommand: any
+    shareCommand: any
+    deleteCommand: any
+}) {
+    const auth = useAuth()
+
+    const isOwner = graph.creatorId && graph.creatorId === auth.user!.id
+    const canSave = isOwner || requestPermission({
+        user    : auth.user,
+        resource: "Graphs",
+        action  : graph.id ? "update" : "create"
+    })
+
+    return (
+        <div className="row wrap">
+            <div className="col col-0 mb-1">
+                <div className="toolbar flex">
+                    <button
+                        className={ "btn" + (state.showOptions ? " active" : "")}
+                        onClick={() => dispatch({ type: "TOGGLE_OPTIONS" })}
+                        data-tooltip={state.showOptions ? "Hide Options" : "Show Options"}>
+                        <i className="fas fa-cog" />
+                    </button>
+                    <button
+                        className={ "btn" + (state.inspection.enabled ? " active" : "") }
+                        data-tooltip="Inspect elements by click"
+                        onClick={() => dispatch({
+                            type: "MERGE",
+                            payload: {
+                                inspection: {
+                                    ...state.inspection,
+                                    enabled: !state.inspection.enabled,
+                                    // match  : [],
+                                    // context: {}
+                                }
+                            } 
+                        })}>
+                        <i className="fa-solid fa-crosshairs"/>
+                    </button>
+                    <CommandButton { ...copyCommand } label={ "" } />
+                    <CommandButton { ...shareCommand } label={ "" } />
+                    <CommandButton { ...deleteCommand } label={ "" } />
+                    { canSave && <SaveChartButton graph={graph} saving={saving} save={save} /> }
+                </div>
+            </div>
+            <div className="col mb-1" />
+            <div className="col col-0 mb-1">
+                <ViewTypeSelector value={state.viewType} onChange={payload => dispatch({ type: "SET_VIEW_TYPE", payload })} />
+            </div>
+        </div>
+    )
+}
+
+export function ViewTypeSelector({ value, onChange }: { value: DashboardState["viewType"]; onChange: (value: DashboardState["viewType"]) => void }) {
+    return (
+        <div className="toolbar flex">
+            <button
+                className={"btn" + (value === "overview" ? " active" : "")}
+                onClick={() => onChange("overview")}
+                data-tooltip="Show the Chart"
+                style={{ minWidth: "7em" }}
+                >
+                <i className="fas fa-chart-pie" /> Overview
+            </button>
+            <button
+                className={"btn" + (value === "data"     ? " active" : "")}
+                onClick={() => onChange("data")}
+                data-tooltip="Show the Input Data"
+                style={{ minWidth: "7em" }}
+                ><i className="fas fa-th" /> Data
+            </button>
+        </div>
+    )
+}
---
+++ src/components/Dashboard/SaveChartButton.tsx
@@ -0,0 +1,60 @@
+import { useNavigate } from "react-router"
+import { app } from "../../types"
+import MenuButton from "../generic/MenuButton"
+
+
+export function SaveChartButton({
+    saving,
+    graph,
+    save
+}: {
+    graph: Partial<app.View>
+    saving?: boolean
+    save: (props?: Partial<app.View>) => Promise<void>
+}) {
+    const navigate = useNavigate()
+
+    return (
+        <div className="btn">
+            <span className="menu-button-btn pr-1" onClick={() => save()}>
+                <i
+                    className={ saving ? "fas fa-circle-notch fa-spin" : "fas fa-save" }
+                    style={{ width: "1.2em", color: graph.isDraft ? "#E60" : "inherit" }}
+                    onClick={() => save()}
+                /> Save
+            </span>
+            <MenuButton right items={[
+                <div style={{ cursor: "default" }} onClick={() => {
+                    save({ isDraft: false }).then(() => {
+                        if (graph.id) {
+                            navigate(`/views/${graph.id}`)
+                        }
+                    })
+                }}>
+                    <div className="color-grey-dark" style={{ "fontWeight": 500 }}>
+                        { graph.isDraft ? "Publish" : "Save Changes" }
+                    </div>
+                    <div className="small color-muted">
+                        Save changes and make this visible in the graphs list
+                    </div>
+                </div>,
+                <div style={{ cursor: "default" }} onClick={() => {
+                    save({ isDraft: true }).then(() => {
+                        if (graph.id) {
+                            navigate(`/drafts/${graph.id}`)
+                        }
+                    })
+                }}>
+                    <div className="color-brand-2" style={{ "fontWeight": 500 }}>
+                        { graph.isDraft ? "Save Draft" : "Switch to Draft" }
+                    </div>
+                    <div className="small color-muted">
+                        Save changes and hide this from the graphs list
+                    </div>
+                </div>
+            ]}>
+                <i className="fa-solid fa-caret-down small" />
+            </MenuButton>
+        </div>
+    )
+}
