760396c25c7a860138616ede07b0fe813fb8d229
Vladimir Ignatov <vlad.ignatov@gmail.com>
2024-07-09 11:51:40 -0400

    Toggle Show in Legend for all series #50

--- src/components/generic/PropertyGrid/BooleanEditor.tsx
+++ src/components/generic/PropertyGrid/BooleanEditor.tsx
@@ -1,15 +1,35 @@
-import { EditableProperty } from "./types"
+import { useEffect, useRef } from "react"
+import { EditableProperty }  from "./types"
 
 
 export default function BooleanEditor({ prop }: { prop: EditableProperty }) {
     return (
         <label>
-            <input
+            <CheckBox
                 type="checkbox"
                 disabled={ !!prop.disabled }
-                checked={ prop.value }
+                checked={ prop.indeterminate ? undefined : prop.value }
                 onChange={ () => prop.onChange(!prop.value) }
-            />{ prop.value + "" }
+                indeterminate={ prop.indeterminate }
+            />{ prop.indeterminate ? "mixed" : prop.value + "" }
         </label>
     )
 }
+
+
+
+interface CheckBoxProps extends React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
+    indeterminate?: boolean
+}
+
+export function CheckBox(props: CheckBoxProps = {}) {
+    const ref = useRef<HTMLInputElement>(null)
+    const { indeterminate, ...rest } = props
+    useEffect(() => {
+        if (indeterminate !== undefined && ref.current) {
+            ref.current.indeterminate = indeterminate
+        }
+    }, [indeterminate])
+    return <input { ...rest } type="checkbox" ref={ref} />
+}
+
