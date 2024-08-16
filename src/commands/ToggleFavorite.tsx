d386d28d70bbf1c2203100dd9afca46a1b950840
Vladimir Ignatov <vlad.ignatov@gmail.com>
2024-08-16 12:48:22 -0400

    ToggleFavorite command #39

---
+++ src/commands/ToggleFavorite.tsx
@@ -0,0 +1,49 @@
+import { Command } from "./Command"
+
+
+export class ToggleFavorite extends Command
+{
+    private id: string;
+    private group: string;
+
+    /**
+     * @param id ID to be added to or removed from the list
+     * @param group The localStorage key
+     */
+    constructor(id: string | number, group: string) {
+        super()
+        this.id = id + ""
+        this.group = group
+    }
+
+    private getList() {
+        return String(window.localStorage[this.group] || "").trim().split(/\s*,\s*/)
+    }
+
+    active() {
+        return this.getList().includes(this.id)
+    }
+    
+    label() {
+        return this.active() ?
+            "Remove from favorites" :
+            "Add to favorites"
+    }
+
+    icon() {
+        return this.active() ?
+            <i className="fa-regular fa-star" /> :
+            <i className="fa-solid fa-star" />;
+    }
+
+    execute() {
+        let list = this.getList()
+        if (list.includes(this.id)) {
+            list = list.filter(x => x !== this.id)
+        } else {
+            list = [...list, this.id].filter(Boolean)
+        }
+        window.localStorage[this.group] = list.join(",")
+        window.dispatchEvent(new Event('localStorageUpdate'));
+    }
+}
