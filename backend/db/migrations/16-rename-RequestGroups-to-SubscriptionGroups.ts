f7068a80de41a441a57cafb7fd4184bc3066ff10
Vladimir Ignatov <vlad.ignatov@gmail.com>
2024-08-21 12:17:11 -0400

    Fix migrations

--- backend/db/migrations/16-rename-RequestGroups-to-SubscriptionGroups.ts
+++ backend/db/migrations/16-rename-RequestGroups-to-SubscriptionGroups.ts
@@ -1,14 +1,15 @@
+import { QueryTypes } from "sequelize";
 import { MigrationFunction } from "../../types"
 
 
 export const up: MigrationFunction = async ({ context: queryInterface }) => {
     if (!(await queryInterface.tableExists("SubscriptionGroups"))) {
-        await queryInterface.renameTable("RequestGroups", "SubscriptionGroups");
+        await queryInterface.sequelize.query(`ALTER TABLE RequestGroups RENAME TO SubscriptionGroups`, { type: QueryTypes.RAW, raw: true });
     }
 }
 
 export const down: MigrationFunction = async ({ context: queryInterface }) => {
     if (!(await queryInterface.tableExists("RequestGroups"))) {
-        await queryInterface.renameTable("SubscriptionGroups", "RequestGroups");
+        await queryInterface.sequelize.query(`ALTER TABLE SubscriptionGroups RENAME TO RequestGroups`, { type: QueryTypes.RAW, raw: true });
     }
 }
