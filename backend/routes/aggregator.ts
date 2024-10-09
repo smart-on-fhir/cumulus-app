6c0c5a39f7cdcc9350fc525a07f15b5566aad3df
Vladimir Ignatov <vlad.ignatov@gmail.com>
2024-10-09 14:00:03 -0400

    Aggregator proxy caching

--- backend/routes/aggregator.ts
+++ backend/routes/aggregator.ts
@@ -1,4 +1,4 @@
-
+import crypto                         from "crypto"
 import express, { Request, Response } from "express"
 import https, { RequestOptions }      from "https"
 import { rw }                         from "../lib"
@@ -7,6 +7,7 @@ import config                         from "../config"
 
 export const router = express.Router({ mergeParams: true })
 
+const startTime = Date.now()
 
 // These are the only paths we recognize and proxy to the aggregator
 const AGGREGATOR_PATHS = [
@@ -19,6 +20,7 @@ const AGGREGATOR_PATHS = [
 
     // subscriptions
     "/data_packages",
+    "/data_packages/:id",
 
     // chart-data
     "/chart-data/:subscription_name",
@@ -39,12 +41,27 @@ router.get(AGGREGATOR_PATHS, rw(async (req: Request, res: Response) => {
         return res.status(400).type("text").end("The aggregator API is not enabled")
     }
 
-    const { port, hostname } = new URL(baseUrl + req.url);
+    const { port, hostname, href } = new URL(baseUrl + req.url);
+
+    // Cache -------------------------------------------------------------------
+    const cacheKey = crypto.createHash("sha1").update([href, startTime].join("-")).digest("hex");
+
+    res.setHeader('Cache-Control', `max-age=31536000, no-cache`)
+    res.setHeader('Vary', 'Origin, ETag')
+    res.setHeader('ETag', cacheKey)
+
+    let ifNoneMatchValue = req.headers['if-none-match']
+    if (ifNoneMatchValue && ifNoneMatchValue === cacheKey) {
+        res.statusCode = 304
+        return res.end()
+    }
+    // -------------------------------------------------------------------------
 
     const requestOptions: RequestOptions = {
         host: hostname,
         port: port || undefined,
         path: req.url,
+        method: req.method,
         headers: {
             Host: hostname,
             "x-api-key": apiKey,
@@ -52,11 +69,41 @@ router.get(AGGREGATOR_PATHS, rw(async (req: Request, res: Response) => {
         }
     }
 
-    https.get(requestOptions, aggRes => {
-        res.header(aggRes.headers)
-        res.status(aggRes.statusCode!)
-        aggRes.pipe(res)
+    const proxy = https.request(requestOptions, aggRes => {
+
+        let data = '';
+
+        // Receive chunks of data from the external server
+        aggRes.on('data', (chunk) => {
+            data += chunk; // Buffer the data
+        });
+
+        // On response completion
+        aggRes.on('end', () => {
+            // console.log("%s GOT: %s %s", req.url, aggRes.statusCode, data)
+            try {
+                // Attempt to parse the data as JSON (if the content is JSON)
+                const jsonResponse = JSON.parse(data);
+
+                // Send the JSON response to the client
+                res.writeHead(aggRes.statusCode!, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify(jsonResponse));
+            } catch (error) {
+                // If the response is not JSON, return it as a plain string
+                res.writeHead(aggRes.statusCode!, { 'Content-Type': 'text/plain' });
+                res.end(data);
+            }
+        });
     });
+
+    // Handle any error during the proxy request
+    proxy.on('error', (err) => {
+        console.error('Error with proxy request:', err);
+        res.writeHead(500);
+        res.end('Internal Server Error');
+    });
+
+    req.pipe(proxy);
 }))
 
 export default router
