6d328c012d7163ef24da091dbc2fbe1942a5a1b5
Vladimir Ignatov <vlad.ignatov@gmail.com>
2024-10-09 13:55:45 -0400

    New subscription icons

--- src/components/Subscriptions/SubscriptionLink.tsx
+++ src/components/Subscriptions/SubscriptionLink.tsx
@@ -5,16 +5,6 @@ import { app }       from "../../types"
 import { ToggleFavorite } from "../../commands/ToggleFavorite"
 import { useCommand } from "../../hooks"
 
-function Icon(props: { type: "ok"|"pending"|"working" }) {
-    return (
-        <div className="icon">
-            <i className="fa-solid fa-database"/>
-            { props.type === "pending" && <i className="fas fa-clock"/> }
-            { props.type === "working" && <i className="fas fa-sync-alt"/> }
-            { props.type === "ok" && <i className="fas fa-check-circle"/> }
-        </div>
-    )
-}
 
 export default function SubscriptionLink({
     request,
@@ -31,38 +21,27 @@ export default function SubscriptionLink({
     const toggleFavorite    = useCommand(ToggleFavoriteCmd)
     const on = ToggleFavoriteCmd.on()
 
-    
-    // let type = request.refresh === "manually" ? "REQUEST" : "SUBSCRIPTION"
-    let info = null
-    let iconType: "ok"|"pending"|"working" = "ok"
-    
-    if (!request.completed) {
-        info = <div className="color-muted small">PENDING SUBSCRIPTION</div>
-        iconType = "pending"
-    }
-    
-    else if ([/*"none", "manually", */"daily", "weekly", "monthly", "yearly"].includes(request.refresh)) {
-        info = (<div className="color-muted small">
-            Last refreshed on <Format value={request.completed} format="date" />
-        </div>)
-        iconType = "working"
-    }
-    else {
-        info = (<div className="color-muted small">
-            Completed <Format value={request.completed} format="date" />
-        </div>)
-        iconType = "ok"
-    }
-
     return (
         <Link to={ href.replace(":id", id) } className="icon-item" onContextMenu={e => {
             // @ts-ignore
             e.nativeEvent.menuItems = [toggleFavorite]
         }}>
-            <Icon type={iconType} />
-            <b className="nowrap">{
-                on && <i className="fa-solid fa-star star"/> } <span className="wrap">{ search ? highlight(request.name, search) : request.name }</span></b>&nbsp;
-            {info}
+            <div className="icon">
+                { request.metadata?.type === "flat" ?
+                    <span className="material-symbols-outlined color-blue">table</span> :
+                    !!request.dataURL ?
+                        <span className="material-symbols-outlined color-blue">database</span> :
+                        request.completed ?
+                            <span className="material-symbols-outlined color-blue">news</span> :
+                            <span className="material-symbols-outlined">overview</span> }
+                { on && <i className="fa-solid fa-star star"/> }
+            </div>
+            <b className="nowrap">
+                <span className="wrap">{ search ? highlight(request.name, search) : request.name }</span>
+            </b>
+            <div className="color-muted small">
+                { request.completed ? <>Data updated: <Format value={request.completed} format="date" /></> : "No data yet" }
+            </div>
         </Link>
     )
 }
