b61504aad37f4aa45e3fc2d16b18ee01c10cdb80
Vladimir Ignatov <vlad.ignatov@gmail.com>
2025-01-28 15:50:58 -0500

    Improvements for templates

--- src/components/Dashboard/CreateView.tsx
+++ src/components/Dashboard/CreateView.tsx
@@ -53,6 +53,7 @@ export default function CreateView()
                 viewType: state?.chartType || "spline",
                 filters: [],
                 groupBy: "",
+                sortBy: state?.sortBy || "x:asc",
                 limit: state?.limit || 0,
                 chartOptions: {
                     title: {
