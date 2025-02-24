d7d936ace76d87f6bf763942cbc748cea4f0daf3
Vladimir Ignatov <vlad.ignatov@gmail.com>
2025-02-24 11:11:51 -0500

    Small fix for the loader

--- src/components/generic/Loader/index.tsx
+++ src/components/generic/Loader/index.tsx
@@ -1,5 +1,5 @@
 export default function Loader({ msg = "Loading...", style }: { msg?: string, style?: React.CSSProperties }) {
     return (
-        <span style={style}><i className="fas fa-circle-notch fa-spin"/> {msg}</span>
+        <span style={style}><i className="fas fa-circle-notch fa-spin"/>{msg ? " " + msg : ""}</span>
     )
 }
