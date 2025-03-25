60d0e48f39e7154290cd2f8c19d8ec30711f5008
Vladimir Ignatov <vlad.ignatov@gmail.com>
2025-03-25 10:56:57 -0400

    Fix migrations for prod

--- backend/db/migrations/16-rename-RequestGroups-to-SubscriptionGroups.ts
+++ backend/db/migrations/16-rename-RequestGroups-to-SubscriptionGroups.ts
@@ -2,13 +2,17 @@ import { MigrationFunction } from "../../types"
 
 
 export const up: MigrationFunction = async ({ context: queryInterface }) => {
-    if (!(await queryInterface.tableExists("SubscriptionGroups"))) {
+    const oldExists = await queryInterface.tableExists("RequestGroups")
+    const newExists = await queryInterface.tableExists("SubscriptionGroups")
+    if (oldExists && !newExists) {
         await queryInterface.renameTable("RequestGroups", "SubscriptionGroups");
     }
 }
 
 export const down: MigrationFunction = async ({ context: queryInterface }) => {
-    if (!(await queryInterface.tableExists("RequestGroups"))) {
+    const oldExists = await queryInterface.tableExists("RequestGroups")
+    const newExists = await queryInterface.tableExists("SubscriptionGroups")
+    if (!oldExists && newExists) {
         await queryInterface.renameTable("SubscriptionGroups", "RequestGroups");
     }
 }
