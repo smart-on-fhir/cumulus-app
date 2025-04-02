ccc933e2e06c71854670a752d30015c4aa94be1c
Vladimir Ignatov <vlad.ignatov@gmail.com>
2025-04-02 16:31:41 -0400

    Clean up

--- src/components/generic/Markdown/index.tsx
+++ src/components/generic/Markdown/index.tsx
@@ -1,12 +1,12 @@
-import { merge } from "highcharts";
-const BaseMarkdown = require("markdown-to-jsx")
+import { merge }    from "highcharts";
+import BaseMarkdown, { MarkdownToJSX } from "markdown-to-jsx"
 import { Fragment } from "react";
 import "./Markdown.scss"
 
 
-export default function Markdown({ children, options = {} }: {
-    children: string;
-    options?: any//MarkdownToJSX.Options
+export default function Markdown({ children = "", options = {} }: {
+    children?: string;
+    options ?: MarkdownToJSX.Options
 }) {
     return (
         <div className="markdown">
