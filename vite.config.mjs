15e123dd0693ebeb05d7a60aee91b0af954a8bcf
Vladimir Ignatov <vlad.ignatov@gmail.com>
2025-04-04 12:52:23 -0400

    Upgrade to react 19 and add Vite

---
+++ vite.config.mjs
@@ -0,0 +1,48 @@
+import "dotenv/config"
+import { defineConfig } from 'vite'
+import react            from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+
+  return {
+    root   : "./src",
+    plugins: [react()],
+    assetsInclude: ["../icons/*.png"], // Ensure these file types are included
+    build: {
+      outDir               : "../dist/frontend",
+      emptyOutDir          : true,
+      chunkSizeWarningLimit: 600,
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+            return null;
+          }
+        }
+      },
+      // target: 'esnext', // Ensure modern ESM output
+      // commonjsOptions: {
+      //   transformMixedEsModules: true, // Allows processing ESM in CJS
+      // },
+    },
+    server: {
+      port: 3000,
+      host: "0.0.0.0",
+      proxy: {
+        "^/(api)\\b" : {
+          target      : `http://127.0.0.1:4001`,
+          changeOrigin: true,
+          secure      : false,
+        }
+      }
+    },
+    define: {
+      NODE_ENV              : JSON.stringify(process.env.NODE_ENV || "development"),
+      REACT_APP_BACKEND_HOST: JSON.stringify(process.env.REACT_APP_BACKEND_HOST || ""),
+      REACT_APP_NOTEBOOK_URL: JSON.stringify(process.env.REACT_APP_NOTEBOOK_URL || ""),
+    },
+  }
+})
