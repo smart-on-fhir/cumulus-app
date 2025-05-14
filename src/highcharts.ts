1b067f7cb584906da10a870aa95268a98c34a4df
Vladimir Ignatov <vlad.ignatov@gmail.com>
2025-05-14 10:20:44 -0400

    Experimental tree-map chart for catalog

--- src/highcharts.ts
+++ src/highcharts.ts
@@ -13,7 +13,8 @@ import coloraxis        from "highcharts/modules/coloraxis"
 // import cylinder      from "highcharts/modules/cylinder"
 // import funnel        from "highcharts/modules/funnel"
 // import heatmap       from "highcharts/modules/heatmap"
-// import treemap       from "highcharts/modules/treemap"
+import treemap       from "highcharts/modules/treemap"
+// import boost         from "highcharts/modules/boost"
 // import venn          from "highcharts/modules/venn"
 
 // Initialize modules
@@ -29,7 +30,8 @@ accessibility(Highcharts)
 drilldown(Highcharts)
 coloraxis(Highcharts)
 // cylinder(Highcharts)
-// treemap(Highcharts)
+treemap(Highcharts)
+// boost(Highcharts)
 // funnel(Highcharts)
 // heatmap(Highcharts)
 // venn(Highcharts)
